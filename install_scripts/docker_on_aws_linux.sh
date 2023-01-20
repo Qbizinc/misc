19fa92dfc64d5ae9c9d8e5d8362ebb6b8e5df121
jaythomas-qbiz <jay@qbizinc.com>
2023-01-20 13:22:41 -0800

    add docker on aws linux setup script

---
+++ install_scripts/docker_on_aws_linux.sh
@@ -0,0 +1,21 @@
+# run these first
+sudo yum update -y
+sudo amazon-linux-extras install docker # note that this will require a manual y/n to proceed
+
+# then run these
+sudo service docker start
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+
+# log out and log back in
+
+docker info # to confirm installation
+
+# docker compose:
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+docker compose version # to confirm installation
+
