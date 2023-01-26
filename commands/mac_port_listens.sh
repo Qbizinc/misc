0abe2298ebb143707642e7255a81d921347734cd
jaythomas-qbiz <52762507+jaythomas-qbiz@users.noreply.github.com>
2023-01-26 10:54:13 -0800

    Update mac_port_listens.sh

--- commands/mac_port_listens.sh
+++ commands/mac_port_listens.sh
@@ -1,5 +1,7 @@
 # mac command to see what processes are listening on which ports
 # takes a few minutes
 
+# see https://stackoverflow.com/a/4421674 for more explanatory details
+
 sudo lsof -i -P | grep LISTEN | grep :$PORT
 
